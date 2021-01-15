7dcb10b2757da178f622e1810f7d3258ecd221d1
Christian V <lonwyr@yahoo.de>
2021-01-15 20:52:40 +0100

    Origin/comands (#2)
    * adding a package-lock
    
    * removal of redundand app.js
    
    * adding a config.json
    
    * moving commands to a separate file & adding help
    
    * removing comment & improve prefix slicing
    
    * adding empty line at end of files
    
    * also adjusting config.json file ending

--- app.js
+++ app.js
@@ -2,24 +2,38 @@
 console.log("Starting")
 
 const Discord = require("discord.js")
-const client = new Discord.Client()
-const config = {} // require("./config.json")
+const config = require("./config.json")
+const botCommands = require('./commands')
 
 config.token = process.env.BOT_SECRET
 
+const bot = new Discord.Client()
+bot.commands = new Discord.Collection()
+
+
 console.log("use bot token: " + config.token)
 
-client.login(config.token)
+bot.login(config.token)
 
-config.prefix = "!"
+Object.keys(botCommands).map(key => {
+  bot.commands.set(botCommands[key].name, botCommands[key])
+});
 
-client.on("message", (message) => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+bot.on('message', msg => {
+  const args = msg.content.split(/ +/)
+  const content = args.shift().toLowerCase()
+  console.info(`Called command: ${content}`)
 
-  if (message.content.startsWith(config.prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(config.prefix + "foo")) {
-    message.channel.send("bar!");
+  if (!content.startsWith(config.prefix)) return
+
+  const command = content.slice(config.prefix.length) // remove the prelimiter
+
+  if (!bot.commands.has(command)) return
+
+  try {
+    bot.commands.get(command).execute(msg, args)
+  } catch (error) {
+    console.error(error)
+    msg.reply('there was an error trying to execute that command!')
   }
 })
