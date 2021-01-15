aa2ce54d2ef423d289f30116b1965328607f32d5
Christian V <lonwyr@yahoo.de>
2021-01-16 00:53:15 +0100

    adding skill-checks with tap and mods (#8)

--- commands/help.js
+++ commands/help.js
@@ -3,6 +3,7 @@ const config = require("./../config.json")
 const at = require('./at')
 const pa = require('./pa')
 const aw = require('./aw')
+const taw = require('./taw')
 
 const messageReducer = (message, command) => message + '\n**' + config.prefix + command.name + '** ' + command.help
 
@@ -10,7 +11,7 @@ module.exports = {
   name: 'help',
   description: 'provides the needed help',
   execute(msg, args) {
-    const message = [at, pa, aw].reduce(messageReducer, 'Der **Myranor Würfelsklave**.\n' +
+    const message = [at, pa, aw, taw].reduce(messageReducer, 'Der **Myranor Würfelsklave**.\n' +
     'Folgende Befehle werden zur Zeit unterstützt:\n\n' +
     '**' + config.prefix + 'help** um genau diese Nachricht zu lesen.')
 
--- commands/index.js
+++ commands/index.js
@@ -2,5 +2,6 @@ module.exports = {
   help: require('./help'),
   at: require('./at'),
   pa: require('./pa'),
-  aw: require('./aw')
+  aw: require('./aw'),
+  taw: require('./taw')
 }
