b94a2db3ec9d31cddf5b54c47f901dadd80027cf
Christian Voshage <lonwyr@yahoo.de>
2021-01-19 20:31:20 +0100

    enable switch without arguments

--- commands/switch.js
+++ commands/switch.js
@@ -3,13 +3,20 @@ const cache = require('../helper/cache')
 module.exports = {
   name: 'switch',
   description: 'switches the active character slot',
-  help: 'switches the active character',
+  help: '**slot 1** **slot 2** und **Slot** wechselt den charakter-slot. Ohne Wert verrate ich dir den aktuell Aktiven Charakter.',
   execute: async function(msg, args) {
-    const name = await cache.activateSlot(msg.author.id, args[0])
+    const userId = msg.author.id
+    const slot = args[0]
+    if (!slot) {
+      msg.reply(`Charakter '${cache.getName(userId)}' ist aktiv.`)
+      return
+    }
+
+    const name = await cache.activateSlot(userId, slot)
     if (name !== undefined) {
-      msg.reply(`Charakter '${name}' in slot '${args[0]}' ist nun aktiv.`)
+      msg.reply(`Charakter '${name}' in slot '${slot}' ist nun aktiv.`)
     } else {
-      msg.reply(`Kein charakter wurde im slot '${args[0]}' gespeichert.`)
+      msg.reply(`Kein charakter wurde im slot '${slot}' gespeichert.`)
     }
   }
 }
