5ba4bdeca065ed7fe135ff7d412351d4fdb80b29
Christian V <lonwyr@yahoo.de>
2021-01-20 00:02:31 +0100

    Origin/import fix (#29)
    * changing formating of checks
    
    * providing names for named checks & import fix

--- commands/taw.js
+++ commands/taw.js
@@ -8,12 +8,13 @@ module.exports = {
   description: 'Rolls a skill check',
   help: '*[Attribute1 Attribute2, Attribute3] [TaW] [Modifikator]* Würfelt für dich eine Talentprobe.\n\tEin **spezielles Talent** kann auch direkt mit seinem Namen gewürfelt werden, wenn die Attribute mit dem Befehl **wert** vorher gespeichert wurden. Bsp: **/stoffefärben** oder **/sinnesschärfe 5 3** mit dem TaW und der Erschwernis.',
   detailedHelp: '**Attribute** können als Zahlen angegeben werden oder mit den gespeicherten Werten vom **werte** command.\n**TaW** ist eine Zahl\n**Modifikatoren** sind die Erschwernis (positiv) oder die Erleichterung (negativ)\n\n**Mehrere Optionen** stehen zur Verfügung:\n\t*taw* - wirft alle würfel und gibt die 3 Ergebnisse zurück.\n\t*taw [Attribute1 Attribute2, Attribute3]* - sagt, wieviel TaP* auszugeben sind.\n\t*taw [Attribute1 Attribute2, Attribute3] [TaW]* - sagt, ob es geschafft wurde und wieviel TaP* übrig sind.\n\t*taw [Attribute1 Attribute2, Attribute3]* - sagt, wieviel TaP* auszugeben sind.\n\t*taw [Attribute1 Attribute2, Attribute3] [TaW] [Erschwernis/Erleichterung]* - rechnet die Erschwernis/Erleichterung noch mit ein.\n**Erlaubte Talente** sind zu viele für eine Nachricht :) - einfach alles zusammen schreiben und ohne / siehe Beispiele.',
-  execute(msg, args, skillName) {
+  execute(msg, args, skillName, description) {
     const config = {
-      title: 'Talentprobe',
+      title: 'Talent',
       value: 'TaW',
       abb: 'TaP',
-      type: 'skill'
+      type: 'skill',
+      description: description
     }
     talentAndSpellExecute.execute(msg, args, config, skillName)
   }
--- commands/zfw.js
+++ commands/zfw.js
@@ -8,12 +8,13 @@ module.exports = {
   description: 'Rolls a spell check',
   help: '*[Attribute1 Attribute2, Attribute3] [ZfW] [Modifikator]* Würfelt für dich eine Zauberprobe.\n\tEin **spezieller Zauber** kann auch direkt mit seinem Namen gewürfelt werden, wenn die Attribute mit dem Befehl **wert** vorher gespeichert wurden. Bsp: **/dämonisch** oder **/carafei 8 -2** mit dem ZfW und der Erleichterung.',
   detailedHelp: '**Attribute** können als Zahlen angegeben werden oder mit den gespeicherten Werten vom **werte** command.\n**ZfW** ist eine Zahl\n**Modifikatoren** sind die Erschwernis (positiv) oder die Erleichterung (negativ)\n\n**Mehrere Optionen** stehen zur Verfügung:\n\t*zfw* - wirft alle würfel und gibt die 3 Ergebnisse zurück.\n\t*zfw [Attribute1 Attribute2, Attribute3]* - sagt, wieviel ZfP* auszugeben sind.\n\t*zfw [Attribute1 Attribute2, Attribute3] [ZfW]* - sagt, ob es geschafft wurde und wieviel ZfP* übrig sind.\n\t*zfw [Attribute1 Attribute2, Attribute3]* - sagt, wieviel ZfP* auszugeben sind.\n\t*zfw [Attribute1 Attribute2, Attribute3] [ZfW] [Erschwernis/Erleichterung]* - rechnet die Erschwernis/Erleichterung noch mit ein.\n**Erlaubte Zauber**\n\t' + spellNames,
-  execute(msg, args, spellName) {
+  execute(msg, args, spellName, description) {
     const config = {
-      title: 'Zauberprobe',
+      title: 'Zauber',
       value: 'ZfW',
       abb: 'ZfP',
-      type: 'spell'
+      type: 'spell',
+      description: description
     }
     talentAndSpellExecute.execute(msg, args, config, spellName)
   }
