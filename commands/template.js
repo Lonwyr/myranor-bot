e0bba30d06430125bfc9556fc13b64069af6ef80
Christian Voshage <lonwyr@yahoo.de>
2021-01-26 20:42:39 +0100

    adding schriftlicherausdruck and schauspielerei

--- commands/template.js
+++ commands/template.js
@@ -6,10 +6,10 @@ module.exports = {
   help: 'Gibt dir eine Datei, die man editieren kann. **Flüster** mir einfach die ausgefüllte Datei zurück.',
   detailedHelp: '',
   execute(msg, args) {
-    msg.reply("Hier ist dein **Template**.\nFüll es aus, *optional* kannst du nicht aktivierte Talente löschen.\n**Flüster** es mir zurück. Gibst du 1, 2 oder 3 an, schiebe ich es in einen entsprechenden Slot.", {
+    msg.reply("Hier ist dein **Template**.\nFülle es aus, *optional* kannst du nicht aktivierte Talente löschen.\n**Flüster** es mir zurück. Gibst du 1, 2 oder 3 an, schiebe ich es in einen entsprechenden Slot.", {
       files: [
         "./data/import.json"
       ]
-    });
+    })
   }
 }
