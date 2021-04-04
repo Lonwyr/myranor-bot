b9bf071abd401b1501def8afb6dc70aa204e52fa
Christian Voshage <lonwyr@yahoo.de>
2021-04-04 20:42:27 +0200

    add translation and skill grouping

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,11 +1,22 @@
 sap.ui.define([
   "com/lonwyr/MyranorBot/controller/BaseController",
+  "sap/m/GroupHeaderListItem",
   "com/lonwyr/MyranorBot/utils/Roller"
 ], function(
     Controller,
+    GroupHeaderListItem,
     Roller
   ) {
   "use strict";
+
+  const skillCategories = {
+    "KÖRPERLICH": 1,
+    "GESELLSCHAFTLICH": 2,
+    "NATUR": 3,
+    "WISSEN": 4,
+    "SPRACHEN": 5,
+    "HANDWERKLICH": 6
+  }
 
   function getProperty (clickEvent) {
     const bindingContext = clickEvent.getSource().getBindingContext("character");
@@ -23,6 +34,28 @@ sap.ui.define([
   }
 
   return Controller.extend("com.lonwyr.MyranorBot.controller.Main", {
+    formatAttributeName: function (attribute) {
+      return this.getResourceBundle("i18n").getText(attribute);
+    },
+    getGroupHeader: function (group) {
+      return new GroupHeaderListItem({
+				title: group.key,
+				upperCase: false
+			});
+    },
+    compareSkills: function (a, b) {
+      const comparision = skillCategories[a] - skillCategories[b]
+
+      if (comparision < 0) {
+        return -1
+      }
+
+      if (comparision > 0) {
+        return 1
+      }
+
+      return 0
+    },
     onRollAttribute: function (clickEvent) {
       const attribute = getProperty(clickEvent);
       return Roller.checkAttribute(attribute);
