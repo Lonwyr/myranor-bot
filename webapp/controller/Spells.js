4bece013c22daded27442ace3461d38fe8557de1
Christian Voshage <lonwyr@yahoo.de>
2021-05-10 20:01:22 +0200

    adding attribute selection for spells rolls

--- webapp/controller/Spells.js
+++ webapp/controller/Spells.js
@@ -248,7 +248,7 @@ sap.ui.define([
 
       const checkData = {
         name: spell.name, //`${spell.name} (${characterSource.name} - ${spell.instruction})`,
-        attributes: getAttributes(this.getModel("character").getProperty("/attributes"), characterSource.attributes),
+        attributes: characterSource.attributes,
         value: characterSource.value,
         modificators: spell.modificators,
         quality: spell.quality,
@@ -279,10 +279,16 @@ sap.ui.define([
     formatSpellModifierSwitchLabel: function (name, value) {
       return `${name} (${value})`;
     },
+    formatAttributeSelectionItemText: function (attributeName, attributes) {
+      const attributeValue = attributes.find(att => att.name === attributeName);
+      return `${attributeName} (${attributeValue})`;
+    },
 
     onRollSpell: function (clickEvent) {
       spellPopoverPromise.then(oPopover => oPopover.close())
       let checkData = Object.assign({}, this.getModel("check").getData());
+      const characterAttributes = this.getModel("character").getProperty("/attributes")
+      checkData.attributes = checkData.attributes.map(att => characterAttributes.find(characterAttribute => characterAttribute.name === att));
       checkData.modifier = parseInt(checkData.spontaneousModificator) + checkData.spellModificator - checkData.quality;
       checkData.modifier += checkData.modificators.reduce((a, b) => a + (b.enabled ? parseInt(b.value) : 0), 0);
 
